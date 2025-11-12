1889ff28f4a96cc0e9a0e13c062e784fa199cf87
Hamza El Nawawy <mozaelnawawy@gmail.com>
2025-11-12 15:19:09 +0200

    Define Reptile class and include necessary headers
    Added includes for string and iostream, and defined the Reptile class with its members.

--- Reptile.h
+++ Reptile.h
@@ -1,16 +1,20 @@
 #ifndef REPTILE_H
 #define REPTILE_H
+#include'Animal.h'
 
-#include "Animal.h"
+#include <string>
+#include<iostream>
+using namespace std;
 
-//define reptile class here
+class Reptile : public Animal {
+private:
+	bool isVenomous;
+public:
+	Reptile(string name, int age, bool isVenomous, bool isHungry);
+	Reptile();
+	~Reptile();
+	bool getIsVenomous();
+	void setIsVenomous(bool isVenomous);
 
-
-
-
-
-
-
-
-
-#endif
+};
+#endif 
