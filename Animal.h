08070d74f0d95ddd82a3d6a91fe3dccfea7e4550
Hamza El Nawawy <mozaelnawawy@gmail.com>
2025-11-12 15:17:47 +0200

    Define Animal class with attributes and methods
    Added Animal class with attributes and methods for name, age, and hunger status.

--- Animal.h
+++ Animal.h
@@ -1,11 +1,27 @@
 #ifndef ANIMAL_H
 #define ANIMAL_H
-
 #include <string>
+#include<iostream>
 using namespace std;
 
-//define animal class here 
+class Animal {
+protected:
+	string name;
+	int age;
+	bool isHungry;
+public:
+	Animal(string name, int age, bool isHungry);
+	Animal();
+	~Animal();
+	void display();
+	void feed();
+	string getName();
+	int getAge();
+	bool getIsHungry();
+	void setName(string name);
+	void setAge(int age);
+	void setIsHungry(bool isHungry);
 
 
-
-#endif
+};
+#endif 
