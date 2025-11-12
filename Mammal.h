833a3328ef4ff8dbe1b725d894b8fa05368b352b
Hamza El Nawawy <mozaelnawawy@gmail.com>
2025-11-12 15:18:49 +0200

    Enhance Mammal class with furColor attribute
    Added furColor attribute and related methods to Mammal class.

--- Mammal.h
+++ Mammal.h
@@ -1,13 +1,21 @@
 #ifndef MAMMAL_H
 #define MAMMAL_H
+#include'Animal.h'
 
-#include "Animal.h"
+#include <string>
+#include<iostream>
+using namespace std;
 
-//define mammal class here
+class Mammal : public Animal {
+private:
+	string furColor;
+public:
+	Mammal(string name, int age, bool isHungry,string furColor);
+		Mammal();
+		~Mammal();
+	string getFurColor();
+	void setFurColor(string furColor);
+};
+#endif 
 
 
-
-
-
-
-#endif
